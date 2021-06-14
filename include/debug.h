dd79940040703c1702829860c71392738d53f7f7
stevenl4 <stevenl4@vt.edu>
2021-06-14 00:28:12 -0400

    message

--- include/debug.h
+++ include/debug.h
@@ -16,7 +16,6 @@
  * You should have received a copy of the GNU General Public License
  * along with SGX-WALLET.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 #ifndef DEBUG_H_
 #define DEBUG_H_
 
